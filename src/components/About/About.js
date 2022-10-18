f77a007d776e970bd127c15d1ea5bf4f9fc29024
Abir Hasan <cse.170201063@gmail.com>
2022-10-18 19:49:11 +0600

    59.4 Get form data and display form error.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/UserContext';
 
 const About = () => {
+    const user=useContext(AuthContext)
     return (
         <div>
             <h1>about</h1>
+            <p>{user?.email}</p>
         </div>
     );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUp = () => {
+    const [error, setError] = useState(null);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email, password, confirm);
+        if (password.length < 6) {
+            setError('Password must be 6 characters or more.');
+            return;
+        }
+        else {
+            setError(null)
+        }
+        if (password !== confirm) {
+            setError('Your password did not match.')
+            return;
+        } else {
+            setError(null)
+        }
+    }
     return (
       <div className="form-container">
         <h2 className="form-title">Sign Up</h2>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="email">Email</label>
             <input type="email" name="email" required />
@@ -19,11 +41,12 @@ const SignUp = () => {
             <label htmlFor="confirm">Confirm Password</label>
             <input type="password" name="confirm" required />
           </div>
+            <p className="error-text">{error!==null?error:''}</p>
           <input className="btn-submit" type="submit" value="Sign Up" />
         </form>
         <p>
           Already have an account? <Link to="/login">Login</Link>
-        </p>
+            </p>
       </div>
     );
 };
